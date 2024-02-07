030de5d586ee4863cafeaf8aa0291bfbc2ced5b1
Alexander Cleary <alexjacleary@gmail.com>
2024-02-07 18:14:49 +1100

    Begun Styling

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -25,7 +25,7 @@ const answerData = [
     [1, 1, 1, 1],
     [1, 1, 1, 1],
     [1, 1, 1, 1],
-    [1, 1, 1, 1],
+    [163, 324, 232, 123],
 ]
 
 function submitAnswer(answerId) {
@@ -36,10 +36,12 @@ function submitAnswer(answerId) {
 function displayNextQuestion() {
     mainElement.innerHTML = `
     <h2>${questionList[currentQuestion].question}</h2>
-    <button id="answer0">${questionList[currentQuestion].answers[0]}</button>
-    <button id="answer1">${questionList[currentQuestion].answers[1]}</button>
-    <button id="answer2">${questionList[currentQuestion].answers[2]}</button>
-    <button id="answer3">${questionList[currentQuestion].answers[3]}</button>
+    <div class="question-answers">
+        <button id="answer0">${questionList[currentQuestion].answers[0]}</button>
+        <button id="answer1">${questionList[currentQuestion].answers[1]}</button>
+        <button id="answer2">${questionList[currentQuestion].answers[2]}</button>
+        <button id="answer3">${questionList[currentQuestion].answers[3]}</button>
+    </div>
     `
 
     for (let i = 0; i < 4; i++) {
@@ -56,7 +58,7 @@ function displayResults() {
     
     `
     new Chart("pie", {
-        type: "pie",
+        type: "doughnut",
         data: {
             labels: questionList[currentQuestion].answers,
             datasets: [{
@@ -66,13 +68,22 @@ function displayResults() {
                     "#e8c3b9",
                     "#1e7145"
                 ],
-                data: answerData[currentQuestion]
+                data: answerData[currentQuestion],
+                borderWidth: 0
             }]
         },
         options: {
             title: {
+                display: false,
+            },
+            legend: {
                 display: true,
-                text: "Results"
+                position: 'bottom',
+                labels: {
+                    fontColor: 'white',
+                    fontSize: 18,
+                    font: 'Arial',
+                }
             }
         }
     });
@@ -90,9 +101,13 @@ function displayResults() {
 function displayConclusion() {
     mainElement.innerHTML = `
     <h2>Conclusion</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
     `
 }
 
 const mainElement = document.querySelector('main');
 let currentQuestion = 0;
-displayNextQuestion()
+document.getElementById(`start`).addEventListener('click', function() {
+
+    displayNextQuestion();
+});
