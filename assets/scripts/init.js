13d89d98869d3b5676b77de7f60f65721780ffbe
Alexander Cleary <alexjacleary@gmail.com>
2024-02-08 20:52:49 +1100

    Complete Function To Fix Multiple Bugs

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -24,14 +24,11 @@ const answerData = [
 ]
 
 function displayNextQuestion() {
-    for (let i = 0; i <= questionList.length; i++) {
-        if (i >= questionList.length ) {
-            displayConclusion()
-            return;
-        } else if (getCookie(`q${i}`) === undefined) {
-            currentQuestion = i;
-            break;
-        }
+    if (complete()) {
+        displayConclusion()
+        return;
+    } else {
+        currentQuestion = complete(0, "i")
     }
 
     let questionAnswers = ``;
@@ -68,7 +65,7 @@ function displayResults() {
     mainElement.innerHTML = `
     <h2 class="question">${questionList[currentQuestion].question}</h2>
     <canvas id="pie" style="width:100%;max-width:700px"></canvas>
-    <button id="next">${currentQuestion < questionList.length - 1 ? 'Next Question' : 'Finish Survey'}</button>
+    <button id="next">${complete() ? 'Finish Survey' : 'Next Question'}</button>
     `
 
     new Chart("pie", {
@@ -111,8 +108,6 @@ function displayResults() {
     });
 }
 function displayConclusion() {
-    document.cookie = "complete=true"
-
     mainElement.innerHTML = `
     <h2>Conclusion</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -120,8 +115,21 @@ function displayConclusion() {
     clearInterval(countTick);
     countElement.innerHTML = `${questionList.length} Questions`;
 }
+function complete(start=0, mode=`boolean`) {
+    for (let i = start; i <= questionList.length; i++) {
+        if (i >= questionList.length ) {
+            return true;
+        } else if (getCookie(`q${i}`) === undefined) {
+            if (mode === `boolean`) {
+                return false;
+            } else if (mode === 'i') {
+                return i;
+            }
+        }
+    }
+}
 
-function uuidv4() {
+function UUIDv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
         .replace(/[xy]/g, function (c) {
             const r = Math.random() * 16 | 0,
@@ -139,7 +147,7 @@ let userUUID;
 if (document.cookie) {
     userUUID = getCookie('uuid')
 } else {
-    userUUID = uuidv4();
+    userUUID = UUIDv4();
     document.cookie = `uuid=${userUUID};`
 }
 
@@ -150,11 +158,17 @@ let currentQuestion = 0;
 let countTick;
 countElement.innerHTML = `${questionList.length} Questions`;
 
-if (getCookie('complete') !== undefined) {
+if (complete()) {
     mainElement.innerHTML = `
     <h1>Title</h1>
     <p>You have already completed this survey. To see the conclusion, press the "See Conclusion" button.</p>
     <button id="start">See Conclusion</button>
+    `
+} else {
+    mainElement.innerHTML = `
+    <h1>Title</h1>
+    <p>By clicking "Start Survey!", you agree to share your answers for this survey <strong>anonymously</strong> for use in this social study.</p>
+    <button id="start">Start Survey!</button>
     `
 }
 
