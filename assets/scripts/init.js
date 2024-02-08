4aa30f90d043733076a490a369708e02a4b5351a
Alexander Cleary <alexjacleary@gmail.com>
2024-02-08 22:11:07 +1100

    Fixed Bug In displayAnswers

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -113,7 +113,7 @@ function displayConclusion() {
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
     `
     clearInterval(countTick);
-    countElement.innerHTML = `${questionList.length} Questions`;
+    countElement.innerHTML = `Your Answers`;
 }
 function displayAnswers() {
     let answersHTML = ``;
@@ -121,7 +121,7 @@ function displayAnswers() {
         answersHTML += `
             <div>
                 <h3>Q${i + 1}: ${questionList[i].question}</h3>
-                <h4>${getCookie(`q${i}`) ? ` ${questionList[currentQuestion].answers[getCookie(`q${i}`)]}` : 'Not Answered'}</h4>
+                <h4>${getCookie(`q${i}`) ? ` ${questionList[i].answers[getCookie(`q${i}`)]}` : 'Not Answered'}</h4>
             </div>
         `
     }
