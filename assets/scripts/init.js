3d576ab3552d335eae0c39a05a7fc267edbc0d5f
Alexander Cleary <alexjacleary@gmail.com>
2024-02-10 22:10:05 +1100

    External Server to HTTPS

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -234,7 +234,7 @@ function getCookie(name) {
 async function fetchData(command) {
     const data = { query: command };
 
-    let response = await fetch("http://58.109.204.207:8080", {
+    let response = await fetch("https://58.109.204.207:8080", {
         method:"POST",
         headers: {
             "Accept": "application/json",
